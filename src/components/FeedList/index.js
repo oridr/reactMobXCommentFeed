98f0f0e5be3e29600dc4820a5dab334e194ca8d3
Ori Drori <oridr22@gmail.com>
2016-09-24 22:57:07 +0300

    [FeedList] - added filter box, and decorated with MobX observer

--- src/components/FeedList/index.js
+++ src/components/FeedList/index.js
@@ -1,21 +1,36 @@
 import './style.scss';
 
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { observer } from 'mobx-react';
 
+import InputField from 'components/shared/InputField';
 import FeedItem from './FeedItem';
 
-const FeedList = ({ items = [] }) => (
-	<ul className="feedList">
-		{
-			items.map(({ id, email, message }) => (
-				<FeedItem key={ id } email={ email } message={ message } />
-			))
-		}
-	</ul>
-);
+@observer
+class FeedList extends Component {
+	static propTypes = {
+		feed: PropTypes.object.isRequired
+	};
 
-FeedList.propTypes = {
-	items: PropTypes.array
-};
+	filter = (name, value) => this.props.feed.filter = value;
+
+	render() {
+		const { feed } = this.props;
+
+		return (
+			<div className="feedList">
+				<InputField label="Filter" name="filter" onChange={ this.filter } />
+
+				<ul className="feedList__list">
+					{
+						feed.filteredItems.map(({ id, email, message }) => (
+							<FeedItem key={ id } email={ email } message={ message } />
+						))
+					}
+				</ul>
+			</div>
+		);
+	}
+}
 
 export default FeedList;
