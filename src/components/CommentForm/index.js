bd6b60720407adaf04f61755fb237bbe52d526f2
Ori Drori <oridr22@gmail.com>
2016-09-24 22:55:56 +0300

    [CommentForm] - added adding comment logic

--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -4,20 +4,50 @@ import React, { PropTypes, PureComponent } from 'react';
 
 import InputField from 'components/shared/InputField';
 
+const getInitialState = () => ({
+	email: { value: '', valid: false },
+	message: { value: '', valid: false }
+});
+
 class CommentForm extends PureComponent {
+	static propTypes = {
+		addItem: PropTypes.func.isRequired
+	};
+
+	state = getInitialState();
+
+	setItemProp = (name, value, validity) => this.setState({
+		[name]: { value, validity }
+	});
+
+	addItem = (e) => {
+		e.preventDefault();
+
+		const { email, message } = this.state;
+
+		this.props.addItem({
+			email: email.value,
+			message: message.value
+		});
+
+		this.setState(getInitialState());
+	};
+
 	render() {
+		const { email, message } = this.state;
+
 		return (
 			<div className="commentForm">
-				<form>
-					<div className="commentForm__field">
-						<InputField label="Email" name="email" type="email" required onChange={ (name, value) => console.log(name,value) } />
+				<form noValidate onSubmit={ this.addItem }>
+					<div className="commentForm__field commentForm__field--email">
+						<InputField label="Email" name="email" type="email" value={ email.value } required onChange={ this.setItemProp } />
 					</div>
 
-					<div className="commentForm__field">
-						<InputField label="Message" name="messsage" required multi onChange={ (name, value) => console.log(name,value) } />
+					<div className="commentForm__field commentForm__field--message">
+						<InputField label="Message" name="message" value={ message.value } required multi onChange={ this.setItemProp } />
 					</div>
 
-					<button type="submit" className="commentForm__submit">Submit</button>
+					<button type="submit" className="commentForm__submit" disabled={ !email.validity || !message.validity }>SUBMIT</button>
 				</form>
 			</div>
 		);
