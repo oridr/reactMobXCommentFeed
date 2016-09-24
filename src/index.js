85b727ea7c741ae4da23db481c1594414238bdd3
Ori Drori <oridr22@gmail.com>
2016-09-24 22:59:40 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
+import 'styles/normalize.css';
+
 import React from 'react';
 import { render } from 'react-dom';
-import FeedItem from 'components/FeedItem';
+
+import feed from './Feed';
+import App from 'components/App';
 
 render(
-	<FeedItem email="elik@bigpanda.io" message="you are not alone" />,
+	<App feed={ feed } />,
 	document.getElementById('root')
 );
-
-// if (module.hot) {
-// 	module.hot.accept('./App', () => {
-// 		const NextApp = require('./App').default;
-//
-// 		render(
-// 			<AppContainer>
-// 				<NextApp appState={appState} />
-// 			</AppContainer>,
-// 			document.getElementById('root')
-// 		);
-// 	});
-// }
