4e3c21514c84a1a5d497f41fde1efb5b029d9f74
Ori Drori <oridr22@gmail.com>
2016-09-24 22:58:01 +0300

    [App] - recieves MobX feed as props, and pass relevant props to children

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,22 +5,22 @@ import React, { Component, PropTypes } from 'react';
 import CommentForm from 'components/CommentForm';
 import FeedList from 'components/FeedList';
 
-const list = [
-	{ id: 1, email: 'elik@bigpanda.io', message: 'Hello there. How are you?' },
-	{ id: 2, email: 'Shai@bigpanda.io', message: 'Good!!!' },
-	{ id: 3, email: 'noam@bigpanda.io', message: 'Goodbye :}' }
-];
-
 class App extends Component {
+	static propTypes = {
+		feed: PropTypes.object.isRequired
+	};
+
 	render() {
+		const { feed } = this.props;
+
 		return (
 			<div className="app">
 				<div className="app__area app__commentForm">
-					<CommentForm />
+					<CommentForm addItem={ feed.addItem } />
 				</div>
 
 				<div className="app__area app__feed">
-					<FeedList items={ list } />
+					<FeedList feed={ feed } />
 				</div>
 			</div>
 		);
